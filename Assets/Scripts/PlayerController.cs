2cf46cef836a8ae012fc0ec56e424a5df087ac2f
Xing Ye <yxinghang@gmail.com>
2020-11-30 13:32:28 -0500

    added plane, spheres, colors, input movement installed

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.inputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void onMove(InputValue movementValue)
+    {
+
+    }
+
+}
