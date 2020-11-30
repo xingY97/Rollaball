107fec777d0a2dbebd09072717e7c921d6ff5007
Xing Ye <yxinghang@gmail.com>
2020-11-30 13:59:40 -0500

    added force to the movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.inputSystem;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //gets a refrence from rigidbody
+        rb = GetComponent<Rigidbody>();
     }
 
-    void onMove(InputValue movementValue)
+    void OnMove(InputValue movementValue)
     {
+        //this code take the movement value and store it to a vector 2 variable(movemenvector)
+        Vector2 movementVector = movementValue.Get<Vector2> ();
 
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
 }
